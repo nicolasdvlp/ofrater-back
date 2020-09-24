9dc8616d9b56db21c5d97ae33523d01b1200fc97
jego <jego.lucas@gmail.com>
2020-09-24 13:30:29 +0200

    express setup and base architecture setup

---
+++ app/routers/clientRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+/**
+ * Routes /client
+ */
+
+router.get('/', /* controller ici */);
+
+
+
+
+
+
+module.exports = router;
