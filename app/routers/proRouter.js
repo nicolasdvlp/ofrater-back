5ccf152d0cef5e7bce717858b6152ed3ea973230
jego <jego.lucas@gmail.com>
2020-09-29 15:21:53 +0200

    Route to update pro profile

--- app/routers/proRouter.js
+++ app/routers/proRouter.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const proController = require('../controllers/proController');
+
+/**
+ * Routes /pro
+ */
+router.put('/profile', proController.updateProfile);
 
 module.exports = router;
