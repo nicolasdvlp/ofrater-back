4f1ad23ff842ec0b9a916dedfe82928db680745d
nicolas.dvlp <nicolas.dvlp@gmail.com>
2020-10-01 09:38:33 +0200

    before merge

--- app/controllers/proController.js
+++ app/controllers/proController.js
@@ -31,7 +31,11 @@ module.exports = {
     },
 
     async postAvailableAppointment (req, res) {
-// FIXME: a modifier pour la connection pro
+    // FIXME: a modifier pour la connection pro
+
+
+
+
 
 
         const { 
@@ -48,26 +52,25 @@ module.exports = {
         const _toJoinStart = [dateStart, starthour]
         const toJoinStart = _toJoinStart.join(' ')
        
-        const debutDeJ = momenttz.tz(moment(toJoinStart, "YYYY-MM-DD HH:mm"), "Europe/Paris")
-        console.log(debutDeJ, ' debut de journée');
+        let debutDeJ = await momenttz.tz(moment(toJoinStart, "YYYY-MM-DD HH:mm"), "Europe/Paris")
+        // console.log(debutDeJ, ' debut de journée');
 
         // TODO faire horaire de fin de la journée
         const _toJoinEnd = [dateStart, endHour]
         const toJoinEnd = _toJoinEnd.join(' ')
        
         const finDeJ = momenttz.tz(moment(toJoinEnd, "YYYY-MM-DD HH:mm"), "Europe/Paris")
-        console.log(finDeJ, ' fin de journée');
+        // console.log(finDeJ, ' fin de journée');
 
 
         // TODO GENERER DES TIMESTAMPS
 
-        var _timestampsz = [];
 
         // console.log("debutDeJ.add(30, 'm') :", debutDeJ.add(30, 'm'));
         // console.log("debutDeJ :", debutDeJ);
         // console.log("fin de journée - début de journée :");
 
-        const indexDay = await (finDeJ - debutDeJ) /30 / 60 /1000;
+        let indexDay = await (finDeJ - debutDeJ) /30 / 60 /1000 /4;
 
 
 
@@ -94,37 +97,137 @@ module.exports = {
         // }
 
 
+        var _timestampsz = [];
 
 
-        for (let index = 0, p = Promise.resolve(); index < indexDay ; index++) {
-            p = p.then(_ => new Promise(resolve => {
-                
+try {
+    
+
+        _timestampsz.push(debutDeJ);
+        _timestampsz.push('coucou');
+
+
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    _timestampsz.push(dateAugment);
+  }, 1000);
+});
+
+FIXME:
+
+const timestampArray = [];
+const addInArray = async(array, data) => {
+    array.push(data);
+}
+const forLoop = async _ => {
+    for (let index = 0; index < indexDay; index++) {
+        const currentTime = debutDeJ.add(30, 'm').format("YYYY-MM-DDT:HH:mm:SS").toString();
+        addInArray(timestampArray, currentTime);
+    }
+    console.log(timestampArray);
+  }
+forLoop();
+
+
+        console.log('dateAugment2', dateAugment);
+
+        dateAugment =  debutDeJ.add(30, 'm');
+
+        console.log('dateAugment2', dateAugment);
+/*
+
+        do {
+
+            let dateAugment =  debutDeJ.add(30, 'm');
+            console.log(dateAugment);
+            _timestampsz.push(dateAugment);
+            console.log(indexDay)
+            indexDay-=1
+
+        } while ( indexDay >= 0 );
+*/
+        setTimeout(() => {console.log(_timestampsz)}, 1000)
+    
+
+    } catch (error) {
+        console.log(error)
+    }
+    
+// FIXME:
+
+/*
+        const forLoop = async _ => {
+            console.log('Start')
+          
+            for (let index = 0 ; index < indexDay ; index++) {
+
                 if(index ===0){
-                    _timestampsz.push(debutDeJ);
-                resolve();
-
+                    console.log('je suis la');
+                    await _timestampsz.push(await debutDeJ);
                 } else {
-                    _timestampsz.push(debutDeJ.add(30, 'm'));
-                resolve();
-
+                    console.log('ou la');
+                    await _timestampsz.push(new Promise((resolve, reject) => {
+                        resolve(debutDeJ.add(30, 'm'));
+                    }))
                 }
-            })
-        )}
+            }
+
+            console.log('End')
+
+        }
+[
+  Moment<2020-12-01T09:00:00+01:00>,
+  Moment<2020-12-01T09:30:00+01:00>,
+  Moment<2020-12-01T10:00:00+01:00>,
+  Moment<2020-12-01T10:30:00+01:00>,
+  Moment<2020-12-01T11:00:00+01:00>
+]
+        await forLoop()
+
+        const result = await Promise.all(_timestampsz)
+
+        console.log(result);*/
+
 
 
 
         // (async () => {
-        // for (let index = 0;  index < indexDay; index++) {
-            
-        //     if(index ===0){
-        //         await _timestampsz.push(await debutDeJ);
-        //     } else {
-        //         await _timestampsz.push(await debutDeJ.add(30, 'm'));
-        //     }
-        // }
+        //     for (let index = 0, p = Promise.resolve(); index < indexDay ; index++) {
+        //         p = p.then(_ => new Promise(async resolve => {
+        //             console.log(index);
+
+
+        //             if(index ===0){
+        //                 console.log('je suis la');
+        //                 await _timestampsz.push(await debutDeJ);
+        //             } else {
+        //                 console.log('ou la');
+        //                 await _timestampsz.push(await debutDeJ.add(30, 'm'));
+        //             }
+        //             resolve();
+
+        //         })
+        //     )}
         // })();
 
-        console.log(_timestampsz);
+
+/*
+        (async () => {
+        for (let index = 0;  index < indexDay; index++) {
+            
+            if(index ===0){
+                _timestampsz.push(debutDeJ);
+            } else {
+                _timestampsz.push(debutDeJ.add(30, 'm'));
+                console.log(debutDeJ)
+
+            }
+        }
+        })();
+  */      
+
+
 
         // TODO mettre dans un tableau  les timestampz
 
@@ -157,6 +260,3 @@ module.exports = {
     // * methode qui va boucler tous les jours la methode du dessous entre telle date et telle date
         //! methode de prise de plusieurs rdv entre telle heure et telle heure (toutes les heures ou demi heure par ex)
             //? methode de prise de un 1 rdv
-
-
-
