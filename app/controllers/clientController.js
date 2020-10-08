15cf1a36e9f12067200da98a0a51b32dc5eedb2a
nicolas.dvlp <nicolas.dvlp@gmail.com>
2020-10-08 10:52:16 +0200

    correction of isNaN verification

--- app/controllers/clientController.js
+++ app/controllers/clientController.js
@@ -66,10 +66,10 @@ module.exports = {
         if (!oldAappointmentID) { return response.status(400).json({ message: 'missing_required_parameter', info: 'oldAappointmentID' }); };
         if (!userID) { return response.status(400).json({ message: 'missing_required_parameter', info: 'userID' }); };
         if (!serviceID) { return response.status(400).json({ message: 'missing_required_parameter', info: 'serviceID' }); };
-        if (isNaN(newAppointmentID)||newAppointmentID<=0||typeof newAppointmentID !== 'number') { return response.status(400).json({ message: 'newAppointmentID must be a positive number', info: 'newAppointmentID' }); };
-        if (isNaN(oldAappointmentID)||oldAappointmentID<=0||typeof oldAappointmentID !== 'number') { return response.status(400).json({ message: 'oldAappointmentID must be a positive number', info: 'oldAappointmentID' }); };
-        if (isNaN(userID)||userID<=0||typeof userID !== 'number') { return response.status(400).json({ message: 'userID must be a positive number', info: 'userID' }); };
-        if (isNaN(serviceID)||serviceID<=0||typeof serviceID !== 'number') { return response.status(400).json({ message: 'serviceID must be a positive number', info: 'serviceID' }); };
+        if (isNaN(newAppointmentID)||newAppointmentID<=0||typeof isNaN(newAppointmentID) !== 'number') { return response.status(400).json({ message: 'newAppointmentID must be a positive number', info: 'newAppointmentID' }); };
+        if (isNaN(oldAappointmentID)||oldAappointmentID<=0||typeof isNaN(oldAappointmentID) !== 'number') { return response.status(400).json({ message: 'oldAappointmentID must be a positive number', info: 'oldAappointmentID' }); };
+        if (isNaN(userID)||userID<=0||typeof isNaN(userID) !== 'number') { return response.status(400).json({ message: 'userID must be a positive number', info: 'userID' }); };
+        if (isNaN(serviceID)||serviceID<=0||typeof isNaN(serviceID) !== 'number') { return response.status(400).json({ message: 'serviceID must be a positive number', info: 'serviceID' }); };
 
         let newRdv;
         let oldRdv;
@@ -110,9 +110,9 @@ module.exports = {
         if (!user_id) { return response.status(400).json({ message: 'missing_required_parameter', info: 'userID' }); };
         // if (!service_id) { return response.status(400).json({ message: 'missing_required_parameter', info: 'serviceID' }); };
 
-        if (isNaN(appointment_id)||appointment_id<0||typeof appointment_id !== 'number') { return response.status(400).json({ message: 'appointment_id must be a positive number', info: 'appointment_id' }); };
-        if (isNaN(user_id)||user_id<0||typeof user_id !== 'number') { return response.status(400).json({ message: 'userID must be a positive number', info: 'userID' }); };
-        // if (isNaN(service_id)||service_id<0||typeof service_id !== 'number') { return response.status(400).json({ message: 'serviceID must be a positive number', info: 'serviceID' }); };
+        if (isNaN(appointment_id)||appointment_id<0||typeof isNaN(appointment_id) !== 'number') { return response.status(400).json({ message: 'appointment_id must be a positive number', info: 'appointment_id' }); };
+        if (isNaN(user_id)||user_id<0||typeof isNaN(user_id) !== 'number') { return response.status(400).json({ message: 'userID must be a positive number', info: 'userID' }); };
+        // if (isNaN(service_id)||service_id<0||typeof isNaN(service_id) !== 'number') { return response.status(400).json({ message: 'serviceID must be a positive number', info: 'serviceID' }); };
         
         let appointment;
 
