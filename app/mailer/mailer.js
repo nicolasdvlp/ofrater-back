7c50aeaeb60b01a7633f17fe6e32bad744876680
jego <jego.lucas@gmail.com>
2020-10-08 10:45:39 +0200

    Mailer exported

--- app/mailer/mailer.js
+++ app/mailer/mailer.js
@@ -1,26 +1,29 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
+function sendmail (userEmailAdress) {
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.GMAILEMAIL,
+            pass: process.env.GMAILPWD
+        }
+    });
 
-let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.GMAILEMAIL,
-        pass: process.env.GMAILPWD
-    }
-});
+    let mailOptions = {
+        from: 'barbeapapaofrater@gmail.com',
+        to: userEmailAdress,
+        subject: 'Bienvenue sur Ofrater !',
+        text: 'Vous venez de vous inscrire sur la meilleure plateforme de prise de rendez-vous chez un coiffeur/barbier !'
+    };
 
-let mailOptions = {
-    from: 'barbeapapaofrater@gmail.com',
-    to: 'coincoin@yopmail.com',
-    subject: 'Bienvenue sur Ofrater !',
-    text: 'Vous venez de vous inscrire sur la meilleure plateforme de prise de rendez-vous chez un coiffeur/barbier !'
-};
+    transporter.sendMail(mailOptions, function(error, data) {
+        if (error) {
+            console.log('Error occurs', error);
+        } else {
+            console.log('Email sent');
+        }
+    });
+}
 
-transporter.sendMail(mailOptions, function(error, data) {
-    if (error) {
-        console.log('Error occurs', error);
-    } else {
-        console.log('Email sent');
-    }
-});
+module.exports = sendmail;
